a3f21e9f32e6de6546c498672a20541104845472
YangJian <yangjian5077@gmail.com>
2015-08-05 23:44:38 +0800

    add problem 5, but uncomplete

---
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.c
@@ -0,0 +1,42 @@
+/*
+Given a string S, find the longest palindromic substring in S. 
+You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int check1(char* s, int n){
+	if(*(s-n) == *(s+n)){
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
+int check2(char* s, int n){
+	if(*(s-n-1) == *(s+n)){
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
+char* longestPalindrome(char* s) {
+	int length = 0;
+	for(int i=1; *(s+i)!='\0'; i++){
+		length += 1;
+	}
+	
+	for(int i=1; i<=length; i++){
+		for(int j=1; j<=i && j<=(length-i); j++){
+			
+		}
+	}
+	
+	
+	
+	char* r = (char*)malloc(sizeof(char));
+	
+    
+}
